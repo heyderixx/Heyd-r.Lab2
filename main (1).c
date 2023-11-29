7b7c6f27ce1e5c183de6447c76c4f373331f66ef
heyderixx <152401836+heyderixx@users.noreply.github.com>
2023-11-29 17:30:36 +0400

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h> 
+int main () {
+    float sonuc, yukseklik, taban;
+    printf("Taban Alan Girin:\n");
+    scanf("%fa" ,&taban);
+    printf("Yuksekligi Alan Girin:\n");
+    scanf("%f" , &yukseklik);
+    sonuc=(taban*yukseklik)/2;
+    printf("Sonuc : %f\n",sonuc);
+    printf("\n");
+    return main ();
+    
+    
+}
